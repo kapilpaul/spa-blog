e9efe0dd0a0ea934f8626d4d9b82461d4b7de7f1
Kapil Paul <kapilpaul007@gmail.com>
2019-01-23 13:59:12 +0600

    first commit

---
+++ src/components/Loader.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: 'Loader'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/blog/page-title.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="section-title-page area-bg">
+        <div class="area-bg__inner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <h1 class="b-title-page">latest from won</h1>
+                        <div class="b-title-page__info">read the blog</div>
+                        <ol class="breadcrumb">
+                            <li><a href="home.html">home</a></li>
+                            <li class="active">blog</li>
+                        </ol>
+                        <!-- end breadcrumb-->
+                        <div class="ui-decor-1"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'pageTitle'
+  }
+</script>
+
+<style scoped>
+
+</style>
